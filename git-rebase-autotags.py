2b9a03990201547a79507dfff2b7260c5a4eda7e
Benjamin Brabant <brabant.benjamin@gmail.com>
2020-05-11 08:57:39 +0200

    migrate git-rebase-autotags from python 2 to python 3

--- git-rebase-autotags.py
+++ git-rebase-autotags.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 #
 # git-rebase-autotags                                                                  
@@ -7,15 +7,16 @@
 # by git amend or rebase command to move tags from old commit to new one
 
 __author__ = "Benjamin Brabant"
-__copyright__ = "Copyright 2017"
+__copyright__ = "Copyright 202O"
 
 __license__ = "GPL"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __maintainer__ = "Benjamin Brabant"
 __email__ = "brabant.benjamin@gmail.com"
 
 import sys                                                                               
 import subprocess                                                                        
+import distutils.util
 
 args = sys.stdin.read().split()
 commits = zip(*[iter(args)]*2)
@@ -23,31 +24,31 @@ rewrite_cmd = sys.argv[1]
 
 try:
 	stdoutput = subprocess.check_output(['git', 'config', '--get', '--bool', 'rewrite.autotags.enabled'])
-	autotags = stdoutput.splitlines()[0].upper() == 'TRUE'
+	autotags = bool(distutils.util.strtobool(stdoutput.decode().strip()))
 except subprocess.CalledProcessError:
 	autotags = False
 
 try:
 	stdoutput = subprocess.check_output(['git', 'config', '--get', '--bool', 'rewrite.autotags.warnings'])
-	warnings = stdoutput.splitlines()[0].upper() == 'TRUE'
+	warnings = bool(distutils.util.strtobool(stdoutput.decode().strip()))
 except subprocess.CalledProcessError:
 	warnings = True
 
 if not autotags and warnings:
-	print "\033[33mWARNING: rewrite.autotags hook configure in this repository has not been activated\033[0m"
-	print "configure rewrite.autotags by running the following commands in the repository"
-	print "    git config --local --add rewrite.autotags.enabled true"
-	print "    git config --local push.followTags true"
-	print ""
-	print "to disable rewrite.autotags warning, apply the following configuration"
-	print "    git config --local --add rewrite.autotags.warnings false"
-	print ""
+	print("\033[33mWARNING: rewrite.autotags hook configure in this repository has not been activated\033[0m")
+	print("configure rewrite.autotags by running the following commands in the repository")
+	print("    git config --local --add rewrite.autotags.enabled true")
+	print("    git config --local push.followTags true")
+	print("")
+	print("to disable rewrite.autotags warning, apply the following configuration")
+	print("    git config --local --add rewrite.autotags.warnings false")
+	print("")
 
 
 
 if autotags:
-	print "\033[33mWARNING: rewrite.autotags activated, tags will be moved between commits during %(cmd)s\033[0m" % {"cmd": rewrite_cmd}
-	print ""
+	print(f"\033[33mWARNING: rewrite.autotags activated, tags will be moved between commits during {rewrite_cmd}\033[0m")
+	print("")
 
 	for (old, new) in commits:
 		stdoutput = subprocess.check_output(['git', 'tag', '--points-at', old])
